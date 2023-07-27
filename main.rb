beb8a0cafc03982091b72cc071a43a242b915ba2
zbahati <bahatizirimwabagabo@gmai.com>
2023-07-28 00:16:35 +0200

    Add main.rb file

--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
 
+require_relative 'my_list'
+# Sample list
+list = MyList.new([1, 2, 3, 4])
+# Test #all?
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+# Test #any?
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+# Test #filter
+puts(list.filter(&:even?))
